d75a57482a6460b164f14770793427264851b50c
Ekam <bhat0155@algonauinlive.com>
2025-05-22 13:28:48 -0400

    deploy

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,16 @@
 import io from "socket.io-client";
 
+// export const createSocketConnection = () => {
+//   return io("http://localhost:4000");
+
+//   // https://chatbackend-xh84.onrender.com"
+// };
+
 export const createSocketConnection = () => {
-  return io("https://chatbackend-xh84.onrender.com");
+  return io("https://chatbackend-xh84.onrender.com", {
+    transports: ["websocket"], 
+    withCredentials: true,
+  });
 };
 
 export default createSocketConnection;
